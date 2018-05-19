5821d6b4476bc74b007a4f248b4d0cd49a3363bc
florm <florenciamartin05@gmail.com>
2018-05-19 12:32:55 -0300

    elimino espacios en nombre de  carpetas

--- Codigo Fuente/application/controller/controller_registro.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Controller_Registro extends Controller{
-
-    function registrar(){
-        $this->view->generate('registro_view.php', 'template_log_view.php');
-    }
-
-}
--- Codigo Fuente/application/view/footer.php
+++
@@ -1,4 +0,0 @@
-
-    <div class="d-flex justify-content-center">
-        <p class="copyrights">Copyright © <?php echo date("Y"); ?> AF‧Group | Todos los derechos reservados.</p>
-    </div>
