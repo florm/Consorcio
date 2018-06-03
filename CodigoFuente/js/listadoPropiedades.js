f0f2c31c06dc60618d0157da517f822c96850cce
florm <florenciamartin05@gmail.com>
2018-06-03 18:46:57 -0300

    mejoro listas

--- CodigoFuente/js/listadoPropiedades.js
+++ CodigoFuente/js/listadoPropiedades.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
     var dataTable = $("#tablaPropiedades").DataTable({
         "processing": true,
         "serverSide": true,
-
+        "bJQueryUI": true,
+        "bPaginate": false,
+        "bProcessing": true,
         scrollY:        400,
         scrollCollapse: true,
         "ajax": {
