fb1b4f6cd15134f3c104558ba388ce72ae93787e
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2018-06-24 13:05:52 -0300

    deshabilitar propiedades que ya tienen un propietario asignado

--- CodigoFuente/includes/getPropiedades.php
+++ CodigoFuente/includes/getPropiedades.php
@@ -2,15 +2,20 @@
 		$conexion = mysqli_connect('127.0.0.1:3307', 'root', '', 'controlprop');
 
 		$idConsorcio = $_POST['idConsorcio'];
-        $sql = "SELECT id, piso, depto FROM propiedad WHERE" . " " . "idConsorcio = '$idConsorcio'";
-       
+        
+       	$sql = "SELECT id, piso, depto, idPropietario FROM propiedad WHERE idConsorcio = '$idConsorcio'";
         $result = mysqli_query($conexion, $sql);
         echo "<select name='idPropiedad' class='form-control'>";
         echo "<option value='0'>Departamento...</option>";
 		 while($fila = mysqli_fetch_array($result))
 		 {  
+		 	if($fila["idPropietario"] == 0){
 		      echo "<option value='" . $fila["id"].  "'>" .  " Piso " . $fila["piso"] .  " Depto " . $fila["depto"] . "</option>";
+		 	}else{
+		 		echo "<option disabled='' value='" . $fila["id"].  "'>" .  " Piso " . $fila["piso"] .  " Depto " . $fila["depto"] . "</option>";
+		 	}
 		 }
 		 echo "</select>";
 
  ?>
+
